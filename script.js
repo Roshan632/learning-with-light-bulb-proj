51ad1e1f2a9da4db398a20c34f815bc2a7fdf722
Roshan Yadav <rosh45644@gmail.com>
2026-04-18 15:13:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ btn.addEventListener("click",function(){
      if(flag==0){
         
         btn.style.backgroundcolor="Yellow"
-        console.log("Clicked")
+        console.log("Clicked Button")
          flag=1
         
      }else{
         bulb.style.backgroundcolor="transparent"
-       console.log("Again Clicked")
+       console.log("Again  Clicked")
          flag=0
         
 
